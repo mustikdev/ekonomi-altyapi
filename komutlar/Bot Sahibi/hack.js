69c08919d7d9355a738467bb598bd85c0c551e19
Mustik <68866033+mustikdev@users.noreply.github.com>
2022-07-07 05:10:50 +0300

    Delete komutlar directory

--- komutlar/Bot Sahibi/hack.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name:"hack",
-  bkz:["Bot sahibinin olan sunucuda parasına 50000 TL eklemesine sağlar."],
-  code:`
-  $setUserVar[para;$sum[$getUservar[para;$authorID];50000];$authorID]
-  The hack is successful.
-  $onlyForIDs[$botOwnerID;]
-  `
-}
--- komutlar/Genel Komutlar/ödül.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name:"ödül",
-  bkz:["12 saatte bir ödül alarak para kazanabilirsiniz."],
-  code:`
-Kutu açıldı ve \`$random[1000;5000]\` :coin: kazandın :tada:
-$setUserVar[para;$sum[$getUserVar[para;$authorID];$random[10;50]];$authorID]
-$cooldown[12h;\`$replaceText[$replaceText[$replaceText[$replaceText[$parseDate[$getCooldownTime[12h;user;günlük;$authorID];time];s;];hour;saat];minute;dakika];econd;saniye]\` sonra ödülünü alabilirsin.]
-`
-}
